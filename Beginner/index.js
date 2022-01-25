d6e6d5f2b83610473b5a4df0730a6c126e26e8bd
PrintedPaper <Crispycrispapples@gmail.comgit config --gloval user.email>
2022-01-24 17:53:41 -0800

    Test change

--- Beginner/index.js
+++ Beginner/index.js
@@ -1,6 +1,13 @@
 const div = document.querySelector("#myDiv");
+const btn = document.querySelector("button")
 
 div.addEventListener("click", e => {
     div.style.backgroundColor = "white";
-    div.style.backgroundColor = "blue"
+    div.style.backgroundColor = "blue";
 }) 
+btn.addEventListener("click", e => {
+    btn.style.backgroundColor = "blue";
+})
+btn.addEventListener("dblclick", e => {
+    btn.style.backgroundColor = "red";
+})
